84ffe678c4f77293caf8291ebe0271cac81a8992
Kedar Patel <kedar29112007@gmail.com>
2026-05-07 23:47:58 +0530

    Basic maths

--- src/Patterns.java
+++ src/Patterns.java
@@ -2,9 +2,9 @@ public class Patterns {
     public static void main(String[] args) {
 
         Algorithms algorithms = new Algorithms();
-        int n = 5;
+        int n = 6;
 
-        algorithms.halfDiamond(n);
+        algorithms.numberPattern(n);
     }
 }
 
@@ -163,6 +163,235 @@ class Algorithms {
             System.out.println();
 
             spaces -= 2;
+        }
+    }
+
+    void numberRightAngle(int n) {
+
+        int start = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i;  j++) {
+                System.out.print(start + " ");
+                start += 1;
+            }
+            System.out.println();
+        }
+    }
+
+    void alphaRightAngle(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (char ch = 'A'; ch <= 'A' + i; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    void upsideDownAlphaRightAngle(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (char ch = 'A'; ch <= 'A' + n - i - 1; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    void rowAlpaRightAngle(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            char ch = 'A';
+            for (int j = 1; j <= ch + i; j++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    void alphaHillPattern(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            char ch = 'A';
+            int breakpoint = (2 * i + 1) / 2;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("-");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print(ch);
+
+                if (j < breakpoint) {
+                    ch++;
+                }
+                else {
+                    ch--;
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    void alphaTrianglePattern(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (char ch = (char) ('A' + n - 1 - i); ch <= (char) ('A' + n - 1); ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    void symmetricVoidPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < 2 * i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < 2 * n - 2 * i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            /*
+             ********** [5, 0, 5]
+             ****  **** [4, 1, 4]
+             ***    *** [3, 4, 3]
+             **      ** [2, 6 ,2]
+             *        * [1, 8, 1]
+             *        * [1, 8, 1] i = 1, n = 5, (2 * n - 2 * i)
+             **      ** [2, 6 ,2]
+             ***    *** [3, 4, 3]
+             ****  **** [4, 1, 4]
+             ********** [5, 0, 5]
+             */
+        }
+    }
+
+    void symmetricButterflyPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < 2 * n - (2 * i + 2); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < 2 * i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    /*
+     *        * [1, 8, 1] 0
+     **      ** [2, 6, 2] 1
+     ***    *** [3, 4, 3] 2
+     ****  **** [4, 2, 4] 3
+     ********** [5, 0, 5] 4
+     ****  **** [4, 2, 4]
+     ***    *** [3, 4, 3]
+     **      ** [2, 6, 2]
+     *        * [1, 8, 1]
+     */
+    }
+
+    void hollowRectanglePattern(int n) {
+
+        // my approach
+//        for (int i = 0; i < n; i++) {
+//            int spaces = n - 2;
+//            if (i == 0 || i == n - 1) {
+//                for (int j = 0; j < n; j++) {
+//                    System.out.print("*");
+//                }
+//                System.out.println();
+//            }
+//            if (i < spaces) {
+//                System.out.print("*");
+//                for (int j = 0; j < spaces; j++) {
+//                    System.out.print(" ");
+//                }
+//                System.out.print("*");
+//                System.out.println();
+//            }
+//        }
+
+        // striver's sheet approach
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        /*
+        n = 4 |
+        **** [2, 0, 2]
+        *  * [1, 2, 1]
+        *  * [1, 2, 1]
+        **** [2, 0, 2]
+
+        n = 5
+        ***** [2, 0, 3]
+        *   * [1, 3, 1]
+        *   * [1, 3, 1]
+        *   * [1, 3, 1]
+        ***** [2, 0, 3]
+         */
+    }
+
+    void numberPattern(int n) {
+        for (int i = 0; i < 2 * n - 1; i++) {
+            for (int j = 0; j < 2 * n - 1; j++) {
+
+                int top = i;
+                int left = j;
+                int bottom = (2 * n - 2) - i;
+                int right = (2 * n - 2) - j;
+
+                int minDistance  = Math.min(Math.min(top, bottom), Math.min(left, right));
+
+                System.out.print(n - minDistance + "  ");
+            }
+            System.out.println();
         }
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,93 @@
-class Test {
+public class Test {
     public static void main(String[] args) {
 
-        int n = 5;
+        int n = -1;
 
-        for (int i = 1; i <= 2 * n - 1; i++) {
-            int stars = i;
-            for (int j = 0; j < stars; j++) {
-                if (i > n) {
-                    stars = 2 * n - i;
-                }
-                System.out.print("*");
+        int count = countDigits(n);
+        int reverse = reverseOfNumber(n);
+        boolean palindrome = isPalindrome(n);
+        int gcd = gcd(12, 8);
+        boolean armstrong = isArmstrong(n);
+
+        System.out.println(armstrong);
+    }
+
+    public static int countDigits(int n)  {
+        return (n == 0) ? 1 : (int) Math.log10(Math.abs(n)) + 1;
+    } // O(1)
+
+    public static int reverseOfNumber(int n) {
+        int rev_num = 0;
+        int rem;
+
+        while (n != 0) {
+            rem = n % 10;
+
+            if (rev_num > Integer.MAX_VALUE / 10 || rev_num == Integer.MAX_VALUE / 10 && rem > 7) {
+                // Edge case: Integer overflow
+                return 0;
             }
-            System.out.println();
+            if (rev_num < Integer.MIN_VALUE / 10 || rev_num == Integer.MIN_VALUE / 10 && rem < -8) {
+                // Edge case: Integer underflow
+                return 0;
+            }
+
+            rev_num = (rev_num * 10) + rem;
+            n /= 10;
         }
+        return rev_num;
+    } // O(log10(n))
+
+    public static boolean isPalindrome(int n) {
+        int rev_num = 0;
+        int rem;
+        int temp = n;
+
+        while (n != 0) {
+            rem = n % 10;
+
+            if (rev_num > Integer.MAX_VALUE / 10 || rev_num == Integer.MAX_VALUE / 10 && rem > 7) {
+                // Edge case: Integer overflow
+                return false;
+            }
+            if (rev_num < Integer.MIN_VALUE / 10 || rev_num == Integer.MIN_VALUE / 10 && rem < -8) {
+                // Edge case: Integer underflow
+                return false;
+            }
+
+            rev_num = (rev_num * 10) + rem;
+            n /= 10;
+        }
+        return temp == rev_num;
+    } // O(log10(n))
+
+    public static int gcd(int num1, int num2) {
+        num1 = Math.abs(num1);
+        num2 = Math.abs(num2);
+
+        while (num1 > 0 && num2 > 0) {
+            if (num1 > num2)  num1 = num1 % num2;
+            else num2 = num2 % num1;
+        }
+
+        if (num1 == 0) return num2;
+        return num1;
+    } // O(log(min(num1, num2)))
+
+    public static boolean isArmstrong(int n) {
+        int digits;
+        int armstrong = 0;
+        int rem;
+        int temp = n;
+
+        digits = (int)Math.log10(Math.abs(n)) + 1;
+        while (n != 0) {
+            rem = n % 10;
+            armstrong += (int) Math.pow(rem, digits);
+            n /= 10;
+        }
+
+        return armstrong == temp;
     }
 }
 
